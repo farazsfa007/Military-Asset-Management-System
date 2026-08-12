1756e09de5afe7ac8d1a4a902ebc072d513c9e1f
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2026-08-12 19:39:06 +0530

    ..

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api"
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://military-asset-management-system-zr1i.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
